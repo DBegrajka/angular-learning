827037bad476d9eda495888dbcea645a573a3ec2
Begrajka, Deepak <Deepak.Begrajka@spiritcom.com>
2019-05-30 10:49:59 -0400

    new changes

---
+++ module1-solution/app.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController',  LunchCheckController);
+  LunchCheckController.inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.checkMenu = function () {
+      var menu = $scope.lunchMenu;
+      if(!menu){
+        $scope.message = 'Please enter data first';
+      }
+      else{
+        var arr = menu.split(',');
+        if(arr.length > 3) {
+          $scope.message = 'Too much!';
+        }
+        else{
+          $scope.message = 'Enjoy!';
+        }
+      }
+    };
+  }
+})();
